b531d54c12bdd301c9efff7e2cd63a4884cbf1c8
FJohnHRoss <rossjohn936@gmail.com>
2022-10-15 22:09:08 -0700

    update

---
+++ week2/Day2/Likes/script.js
@@ -0,0 +1,48 @@
+// var neilCount = 9;
+// var nicholeCount = 12;
+// var jimCount = 9;
+// var neil = document.querySelector("#likes1")
+// var nichole = document.querySelector("#likes2")
+// var jim = document.querySelector("#likes3")
+
+
+// function nielLike() {
+//   neilCount++
+//   neil.innerText = neilCount + " like(s)"
+//   console.log(neilCount)
+// }
+
+// function nicholeLike() {
+//   nicholeCount++
+//   nichole.innerText = nicholeCount + " like(s)"
+//   console.log(nicholeCount)
+// }
+
+// function jimLike() {
+//   jimCount++
+//   jim.innerText = jimCount + " like(s)"
+//   console.log(jimCount)
+// }
+
+var likes = [9, 12, 9];
+
+function nielLike() {
+  var neil = document.querySelector("#likes1");
+  likes[0]++;
+  neil.innerText = likes[0] + " like(s)";
+  console.log(neilCount);
+}
+
+function nicholeLike() {
+  var nichole = document.querySelector("#likes2");
+  likes[1]++;
+  nichole.innerText = likes[1] + " like(s)";
+  console.log(nicholeCount);
+}
+
+function jimLike() {
+  var jim = document.querySelector("#likes3");
+  likes[2]++;
+  jim.innerText = likes[2] + " like(s)";
+  console.log(jimCount);
+}
