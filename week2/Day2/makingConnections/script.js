cba0e277ecb66baaaddc1341d9e14bd4816eb6d1
FJohnHRoss <rossjohn936@gmail.com>
2022-10-17 18:35:16 -0700

    update

--- week2/Day2/makingConnections/script.js
+++ week2/Day2/makingConnections/script.js
@@ -8,35 +8,18 @@ function editLink() {
 
 // ==================== REQUESTS =====================
 
-function acceptRequest1(sdfdf, afawfa) {
-  var div = document.getElementById("requests-1")
+function request(param, type) {
+  var changeDiv = document.getElementById(param)
   var requestText = document.getElementById("request-change")
   var connectionText = document.getElementById("connection-change")
-  requestText.innerText++
-  connectionText.innerText++
-  console.log(requestText)
-  div.remove()
-}
-
-function acceptRequest2() {
-  var div = document.getElementById("requests-2")
-  var requestText = document.getElementById("request-change")
-  var connectionText = document.getElementById("connection-change")
-  requestNum--
-  connectionNum++
-  div.remove()
-}
-
-function declineRequest1() {
-  var div = document.getElementById("requests-1")
-  var requestText = document.getElementById("request-change")
-  requestNum--
-  div.remove()
-}
-
-function declineRequest2() {
-  var div = document.getElementById("requests-2")
-  var requestText = document.getElementById("request-change")
-  requestNum--
-  div.remove()
+  if (type == "accept") {
+    requestText.innerText--
+    connectionText.innerText++
+    console.log(requestText)
+    changeDiv.remove();
+  } else {
+    requestText.innerText--
+    console.log(requestText)
+    changeDiv.remove();
+  }
 }
