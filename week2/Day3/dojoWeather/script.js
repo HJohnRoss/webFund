bd793958fcb378170a5b75034803d65b62b779f1
FJohnHRoss <rossjohn936@gmail.com>
2022-10-18 23:28:32 -0700

    update

--- week2/Day3/dojoWeather/script.js
+++ week2/Day3/dojoWeather/script.js
@@ -4,9 +4,9 @@ function chooseTemp(elem) {
   var num = document.querySelectorAll('.val')
   for (var i = 0; i < num.length; i++) {
     if (elem.value == "°F") {
-      num[i].innerText = (parseInt(num[i].innerText) * (9 / 5) + 32).toFixed(1)
+      num[i].innerText = (parseInt(num[i].innerText) * (9 / 5) + 32).toFixed(0)
     } else {
-      num[i].innerText = ((parseInt(num[i].innerText) - 32) * 5 / 9).toFixed(1)
+      num[i].innerText = ((parseInt(num[i].innerText) - 32) * 5 / 9).toFixed(0)
     }
   }
 }
