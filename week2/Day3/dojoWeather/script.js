92fdc90ffff2e2d3d77958082bc9e31dd91bad51
FJohnHRoss <rossjohn936@gmail.com>
2022-10-18 17:42:13 -0700

    update

--- week2/Day3/dojoWeather/script.js
+++ week2/Day3/dojoWeather/script.js
@@ -2,8 +2,6 @@
 
 function chooseTemp(elem) {
   var num = document.querySelectorAll('.val')
-  console.log(num[0])
-  console.log(typeof (num[0].innerText))
   for (var i = 0; i < num.length; i++) {
     if (elem.value == "°F") {
       num[i].innerText = (parseInt(num[i].innerText) * (9 / 5) + 32).toFixed(0)
